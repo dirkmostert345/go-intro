8fd5a40a8cd43478b3e63d1da311b84ff448f4d0
Dirk Mostert <dirk345@gmail.com>
2021-08-15 11:01:07 +0200

    updated the tutorial to output json

--- Tutorial/hello/hello.go
+++ Tutorial/hello/hello.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"log"
 
+	"encoding/json"
+
 	"example.com/greetings"
 )
 
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
+
 	names := []string{"Gladys", "Samantha", "Darrin"}
+
 	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(messages)
+
+	output, jsonerr := json.Marshal(messages)
+
+	if jsonerr != nil {
+		fmt.Printf("Error: %s", jsonerr.Error())
+	} else {
+		fmt.Println(string(output))
+	}
 }
